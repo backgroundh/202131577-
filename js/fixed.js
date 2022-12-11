8b2ae71677209c528503b76962a039c0755dffe9
backgroundh <bgh4350@gmail.com>
2022-12-11 14:36:50 +0900

    22.12.11

---
+++ js/fixed.js
@@ -0,0 +1,23 @@
+// alert("테스트");
+$(function(){
+    $('#pop_bnt>a>img').click(
+        function(){
+            $('.on').hide();
+        }
+    );  
+    // $('#logo').click(
+    //     function(){
+    //         $('#logo').hide();
+    //     }
+    // );
+    // $("#pop_bnt > a").click(
+    //     function(){
+    //         $('.on').hide()
+    //     }
+    // );
+    // $('nav>ul>li').hover(
+    // function(){
+    //     $('#subNav').stop().slideDown();
+    // }
+    // );
+});
